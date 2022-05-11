44fcf595a75226332f1000161ddcbf882e273e69
Nicholas Rosati <nicholas@rosati.xyz>
2022-05-11 14:27:08 -0400

    connected models
    need to fix a reference to invalid model "minecraft:pipe"

--- src/main/java/xyz/rosati/ipipes/blocks/pipes/PipeBlock.java
+++ src/main/java/xyz/rosati/ipipes/blocks/pipes/PipeBlock.java
@@ -2,19 +2,19 @@ package xyz.rosati.ipipes.blocks.pipes;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityTicker;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -24,37 +24,60 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
-import xyz.rosati.ipipes.iPipes;
 
-public class PipeBlock extends BlockWithEntity implements Waterloggable, BlockEntityProvider {
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("deprecation")
+public class PipeBlock extends Block implements Waterloggable {
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    public static final BooleanProperty NORTH = BooleanProperty.of("north");
+    public static final BooleanProperty SOUTH = BooleanProperty.of("south");
+    public static final BooleanProperty EAST = BooleanProperty.of("east");
+    public static final BooleanProperty WEST = BooleanProperty.of("west");
+    public static final BooleanProperty UP = BooleanProperty.of("up");
+    public static final BooleanProperty DOWN = BooleanProperty.of("down");
+    public static final Map<Direction, BooleanProperty> PROP_MAP = Util.make(new HashMap<Direction, BooleanProperty>(), map->{
+        map.put(Direction.NORTH, NORTH);
+        map.put(Direction.SOUTH, SOUTH);
+        map.put(Direction.EAST, EAST);
+        map.put(Direction.WEST, WEST);
+        map.put(Direction.UP, UP);
+        map.put(Direction.DOWN, DOWN);
+    });
 
     public PipeBlock() {
-        super(FabricBlockSettings.of(Material.STONE)
+        super(FabricBlockSettings.of(Material.GLASS)
                                  .hardness(4.0f)
                                  .nonOpaque());
 
         setDefaultState(this.stateManager.getDefaultState()
-                                         .with(Properties.HORIZONTAL_FACING, Direction.NORTH)
-                                         .with(WATERLOGGED, false));
+                                         .with(WATERLOGGED, false)
+                                         .with(NORTH, false)
+                                         .with(SOUTH, false)
+                                         .with(EAST, false)
+                                         .with(SOUTH, false)
+                                         .with(UP, false)
+                                         .with(DOWN, false));
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        // TODO: outline shape with connections
+//        VoxelShapes.union
         return VoxelShapes.cuboid(4/16f, 4/16f, 4/16f, 1f - 4/16f, 1f - 4/16f, 1f - 4/16f);
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(Properties.HORIZONTAL_FACING, WATERLOGGED);
+        builder.add(WATERLOGGED, NORTH, EAST, SOUTH, WEST, UP, DOWN);
     }
 
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState()
-                   .with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite())
-                   .with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
+        return makeConnections(ctx.getWorld(), ctx.getBlockPos())
+                .with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
     }
 
     @Override
@@ -66,39 +89,40 @@ public class PipeBlock extends BlockWithEntity implements Waterloggable, BlockEn
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         if (state.get(WATERLOGGED)) world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
 
-        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+        Boolean value = isConnectable(world, neighborPos, direction.getOpposite());
+
+        return state.with(getProperty(direction), value);
     }
 
-    @Nullable
-    @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new PipeBlockEntity(pos, state);
+    private boolean isConnectable(WorldAccess world, BlockPos pos, Direction dir) {
+        Block block = world.getBlockState(pos).getBlock();
+
+        return block instanceof PipeBlock || block instanceof AbstractChestBlock || world.getBlockEntity(pos) instanceof Inventory;
+    }
+
+    public Property<Boolean> getProperty(Direction facing) {
+        return PROP_MAP.get(facing);
+    }
+
+    private BlockState makeConnections(World world, BlockPos pos) {
+        Boolean north = isConnectable(world, pos.north(), Direction.SOUTH);
+        Boolean east = isConnectable(world, pos.east(), Direction.WEST);
+        Boolean south = isConnectable(world, pos.south(), Direction.NORTH);
+        Boolean west = isConnectable(world, pos.west(), Direction.EAST);
+        Boolean up = isConnectable(world, pos.up(), Direction.DOWN);
+        Boolean down = isConnectable(world, pos.down(), Direction.UP);
+
+        return this.getDefaultState()
+                   .with(NORTH, north)
+                   .with(EAST, east)
+                   .with(SOUTH, south)
+                   .with(WEST, west)
+                   .with(UP, up)
+                   .with(DOWN, down);
     }
 
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
-    }
-
-    @Nullable
-    @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        return checkType(type, iPipes.PIPE_BLOCK_ENTITY, PipeBlockEntity::tick);
-    }
-
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (world.isClient()) return ActionResult.SUCCESS;
-        PipeBlockEntity be = (PipeBlockEntity) world.getBlockEntity(pos);
-
-        if (be == null) return ActionResult.FAIL;
-
-        if (player.getStackInHand(hand).isEmpty()){
-            player.sendMessage(new LiteralText("Count: " + be.getCount()), false);
-        } else {
-            be.increment();
-        }
-
-        return ActionResult.SUCCESS;
     }
 }
