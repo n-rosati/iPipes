2509fc6296072599cebeec12aceba9548135a7a6
Nicholas Rosati <nicholas@rosati.xyz>
2022-05-11 22:06:24 -0400

    connected models
    need to fix a reference to invalid model "minecraft:pipe"

--- src/main/java/xyz/rosati/ipipes/blocks/pipes/PipeBlock.java
+++ src/main/java/xyz/rosati/ipipes/blocks/pipes/PipeBlock.java
@@ -2,7 +2,6 @@ package xyz.rosati.ipipes.blocks.pipes;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.inventory.Inventory;
@@ -11,11 +10,7 @@ import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.Util;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -37,7 +32,7 @@ public class PipeBlock extends Block implements Waterloggable {
     public static final BooleanProperty WEST = BooleanProperty.of("west");
     public static final BooleanProperty UP = BooleanProperty.of("up");
     public static final BooleanProperty DOWN = BooleanProperty.of("down");
-    public static final Map<Direction, BooleanProperty> PROP_MAP = Util.make(new HashMap<Direction, BooleanProperty>(), map->{
+    public static final Map<Direction, BooleanProperty> PROP_MAP = Util.make(new HashMap<>(), map->{
         map.put(Direction.NORTH, NORTH);
         map.put(Direction.SOUTH, SOUTH);
         map.put(Direction.EAST, EAST);
@@ -48,7 +43,7 @@ public class PipeBlock extends Block implements Waterloggable {
 
     public PipeBlock() {
         super(FabricBlockSettings.of(Material.GLASS)
-                                 .hardness(4.0f)
+                                 .strength(0.5f, 0.5f)
                                  .nonOpaque());
 
         setDefaultState(this.stateManager.getDefaultState()
@@ -89,36 +84,34 @@ public class PipeBlock extends Block implements Waterloggable {
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         if (state.get(WATERLOGGED)) world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
 
-        Boolean value = isConnectable(world, neighborPos, direction.getOpposite());
-
-        return state.with(getProperty(direction), value);
+        return state.with(PROP_MAP.get(direction), isConnectable(world, neighborPos));
     }
 
-    private boolean isConnectable(WorldAccess world, BlockPos pos, Direction dir) {
+    /**
+     * checks if the PipeBlock should connect to a neighbouring block
+     * @param world the world
+     * @param pos neighboring block
+     * @return should the PipeBlock connect to the given neighbouring block
+     */
+    private boolean isConnectable(WorldAccess world, BlockPos pos) {
         Block block = world.getBlockState(pos).getBlock();
 
         return block instanceof PipeBlock || block instanceof AbstractChestBlock || world.getBlockEntity(pos) instanceof Inventory;
     }
 
-    public Property<Boolean> getProperty(Direction facing) {
-        return PROP_MAP.get(facing);
-    }
-
+    /** checks each direction of a places PipeBlock to check if it should connect
+     * @param world the world
+     * @param pos position of a PipeBlock
+     * @return BlockState with connections
+     */
     private BlockState makeConnections(World world, BlockPos pos) {
-        Boolean north = isConnectable(world, pos.north(), Direction.SOUTH);
-        Boolean east = isConnectable(world, pos.east(), Direction.WEST);
-        Boolean south = isConnectable(world, pos.south(), Direction.NORTH);
-        Boolean west = isConnectable(world, pos.west(), Direction.EAST);
-        Boolean up = isConnectable(world, pos.up(), Direction.DOWN);
-        Boolean down = isConnectable(world, pos.down(), Direction.UP);
-
         return this.getDefaultState()
-                   .with(NORTH, north)
-                   .with(EAST, east)
-                   .with(SOUTH, south)
-                   .with(WEST, west)
-                   .with(UP, up)
-                   .with(DOWN, down);
+                   .with(NORTH, isConnectable(world, pos.north()))
+                   .with(EAST, isConnectable(world, pos.east()))
+                   .with(SOUTH, isConnectable(world, pos.south()))
+                   .with(WEST, isConnectable(world, pos.west()))
+                   .with(UP, isConnectable(world, pos.up()))
+                   .with(DOWN, isConnectable(world, pos.down()));
     }
 
     @Override
