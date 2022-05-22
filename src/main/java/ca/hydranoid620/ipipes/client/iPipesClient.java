2a1765103ad76f88d238f06278a02cc45b309eb8
Nicholas Rosati <nicholas@rosati.xyz>
2022-05-22 12:33:22 -0400

    unfinished, adding back the glass to the pipes

--- src/main/java/ca/hydranoid620/ipipes/client/iPipesClient.java
+++ src/main/java/ca/hydranoid620/ipipes/client/iPipesClient.java
@@ -1,7 +1,6 @@
 package ca.hydranoid620.ipipes.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
@@ -13,14 +12,17 @@ import static ca.hydranoid620.ipipes.iPipes.*;
 @Environment(EnvType.CLIENT)
 public class iPipesClient implements ClientModInitializer {
     private static final int PIPE_COLOUR = 0x4f4f4f;
+    private static final int PIPE_GLASS_COLOUR = 0x696969;
     private static final int SUPPLIER_PIPE_COLOUR = 0xef2142;
     private static final int REQUESTER_PIPE_COLOUR = 0x3495eb;
 
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(PIPE_BLOCK, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(SUPPLIER_PIPE_BLOCK, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(REQUESTER_PIPE_BLOCK, RenderLayer.getTranslucent());
 
-        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> PIPE_COLOUR, PIPE_BLOCK);
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> tintIndex == 0 ? PIPE_COLOUR : PIPE_GLASS_COLOUR, PIPE_BLOCK);
         ColorProviderRegistry.ITEM.register((stack, tintIndex) -> PIPE_COLOUR, PIPE_BLOCK_ITEM);
 
         ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> SUPPLIER_PIPE_COLOUR, SUPPLIER_PIPE_BLOCK);
