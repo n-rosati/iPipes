7778486ec573a250c15dd8d8aa81d512053d7582
Nicholas Rosati <nicholas@rosati.xyz>
2023-04-03 18:17:21 -0400

    initial method creation

--- src/main/java/lol/hydranoid620/ipipes/routing/GraphCreator.java
+++ src/main/java/lol/hydranoid620/ipipes/routing/GraphCreator.java
@@ -1,6 +1,15 @@
 package lol.hydranoid620.ipipes.routing;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class GraphCreator {
     //TODO: given a node, build the pipe network graph using DFS
+    public static Set<Node> generateGraph(Node node) {
+        HashSet<Node> nodes = new HashSet<>();
+
+        nodes.add(node);
+
+        return nodes;
+    }
 }
