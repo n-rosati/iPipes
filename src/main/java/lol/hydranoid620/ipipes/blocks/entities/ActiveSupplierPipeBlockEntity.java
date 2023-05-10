edde8bc1764df57c1ae6849cdfe21a7bc9c1b6b8
Nicholas Rosati <nicholas@rosati.xyz>
2023-05-10 16:47:47 -0400

    unused properties, missed removing in #1

--- src/main/java/lol/hydranoid620/ipipes/blocks/entities/ActiveSupplierPipeBlockEntity.java
+++ src/main/java/lol/hydranoid620/ipipes/blocks/entities/ActiveSupplierPipeBlockEntity.java
@@ -2,8 +2,6 @@ package lol.hydranoid620.ipipes.blocks.entities;
 
 import lol.hydranoid620.ipipes.iPipes;
 import lol.hydranoid620.ipipes.routing.Node;
-import lombok.Getter;
-import lombok.Setter;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
@@ -11,14 +9,8 @@ import net.minecraft.world.World;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class ActiveSupplierPipeBlockEntity extends BlockEntity implements IPipeNetworkEndpoint {
-    @Getter
-    private final List<Node> networkConnections = new ArrayList<>();
-    @Setter @Getter
-    private boolean shouldRebuildPaths = true;
-
     public ActiveSupplierPipeBlockEntity(BlockPos pos, BlockState state) {
         super(iPipes.ACTIVE_SUPPLIER_PIPE_BLOCK_ENTITY, pos, state);
     }
