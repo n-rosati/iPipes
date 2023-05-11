0a11499d53a5c373ffd01e7e49685ec451840933
Nicholas Rosati <nicholas@rosati.xyz>
2023-05-11 18:08:22 -0400

    add language data generation fixes #2

---
+++ src/main/java/lol/hydranoid620/ipipes/datagen/EnglishLangProvider.java
@@ -0,0 +1,29 @@
+package lol.hydranoid620.ipipes.datagen;
+
+import lol.hydranoid620.ipipes.iPipes;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+
+public class EnglishLangProvider extends FabricLanguageProvider {
+    public EnglishLangProvider(FabricDataGenerator dataGenerator) {
+        super(dataGenerator, "en_us");
+    }
+
+    /**
+     * Implement this method to register languages.
+     *
+     * <p>Call {@link TranslationBuilder#add(String, String)} to add a translation.
+     *
+     * @param translationBuilder
+     */
+    @Override
+    public void generateTranslations(TranslationBuilder translationBuilder) {
+        translationBuilder.add(iPipes.ITEM_GROUP, "iPipes");
+        translationBuilder.add(iPipes.ACTIVE_SUPPLIER_PIPE_BLOCK, "Active Supplier Pipe");
+        translationBuilder.add(iPipes.PASSIVE_SUPPLIER_PIPE_BLOCK, "Passive Supplier Pipe");
+        translationBuilder.add(iPipes.STORAGE_PIPE_BLOCK, "Storage Pipe");
+        translationBuilder.add(iPipes.REQUESTER_PIPE_BLOCK, "Requester Pipe");
+        translationBuilder.add(iPipes.PIPE_BLOCK, "Pipe");
+        translationBuilder.add(iPipes.NETWORK_CONTROLLER_BLOCK, "Network Controller");
+    }
+}
