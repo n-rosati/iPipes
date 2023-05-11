84c92c3a675f509fb87d679a84716f11a8786bfc
Nicholas Rosati <nicholas@rosati.xyz>
2023-05-11 18:08:22 -0400

    add language data generation fixes #2

---
+++ src/main/java/lol/hydranoid620/ipipes/datagen/DataGeneration.java
@@ -0,0 +1,17 @@
+package lol.hydranoid620.ipipes.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataProvider;
+
+public class DataGeneration implements DataGeneratorEntrypoint {
+    /**
+     * Register {@link DataProvider} with the {@link FabricDataGenerator} during this entrypoint.
+     *
+     * @param fabricDataGenerator The {@link FabricDataGenerator} instance
+     */
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        fabricDataGenerator.addProvider(EnglishLangProvider::new);
+    }
+}
