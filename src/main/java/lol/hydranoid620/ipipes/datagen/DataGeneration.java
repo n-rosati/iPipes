823aa688ac653b94ab7f8f14b14aeab06ab31799
Nicholas Rosati <nicholas@rosati.xyz>
2023-05-11 18:32:08 -0400

    rearrange data gen package

--- src/main/java/lol/hydranoid620/ipipes/datagen/DataGeneration.java
+++ src/main/java/lol/hydranoid620/ipipes/datagen/DataGeneration.java
@@ -1,5 +1,6 @@
 package lol.hydranoid620.ipipes.datagen;
 
+import lol.hydranoid620.ipipes.datagen.languages.EnglishLangProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.data.DataProvider;
