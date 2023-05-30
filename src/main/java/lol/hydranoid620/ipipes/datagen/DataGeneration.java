ceaff19730882241514a9b2a5c044bcdb74a54fe
Nicholas Rosati <nicholas@rosati.xyz>
2023-05-29 23:31:41 -0400

    add datagen for recipes and recipe advancements
    closes #4

--- src/main/java/lol/hydranoid620/ipipes/datagen/DataGeneration.java
+++ src/main/java/lol/hydranoid620/ipipes/datagen/DataGeneration.java
@@ -2,6 +2,7 @@ package lol.hydranoid620.ipipes.datagen;
 
 import lol.hydranoid620.ipipes.datagen.droptables.BlockLootTables;
 import lol.hydranoid620.ipipes.datagen.languages.EnglishLangProvider;
+import lol.hydranoid620.ipipes.datagen.recipes.RecipeGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.data.DataProvider;
@@ -16,5 +17,6 @@ public class DataGeneration implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         fabricDataGenerator.addProvider(EnglishLangProvider::new);
         fabricDataGenerator.addProvider(BlockLootTables::new);
+        fabricDataGenerator.addProvider(RecipeGenerator::new);
     }
 }
